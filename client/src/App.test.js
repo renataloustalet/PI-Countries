1b211efb359351f885aae272118cc04f1d9079b4
renataloustalet <renataloustalet@gmail.com>
2021-11-23 10:58:08 -0300

    Update App.test.js

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,19 +1,19 @@
-import React from "react";
-import { configure, shallow } from "enzyme";
+import React from 'react';
+import { configure, shallow } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
-import { MemoryRouter } from "react-router-dom";
-import configureStore from "redux-mock-store";
-import { Provider } from "react-redux";
+import { MemoryRouter } from 'react-router-dom';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
 
 import Home from "./components/Home/Home";
 import AddActivity from "./components/AddActivity/AddActivity";
 import Landing from "./components/Landing/Landing";
 import Card from "./components/Card/Card";
 
-configure({adapter: new Adapter()})
+configure({ adapter: new Adapter() })
 
 describe('App', () => {
-    let store;
+    let store
     const middlewares = []
     const mockStore = configureStore(middlewares);
 
