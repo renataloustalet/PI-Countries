33255215e0f7abb4235efb72f3bb45a924b2ea52
renataloustalet <renataloustalet@gmail.com>
2021-11-24 20:15:20 -0300

    modifique el form

--- client/src/components/addActivity/AddActivity.jsx
+++ client/src/components/addActivity/AddActivity.jsx
@@ -5,11 +5,26 @@ import { getActivity, getCountries, postActivity } from '../../actions/index'
 import NavDetails from '../Nav/NavDetails';
 import style from './AddActivity.module.css'
 
+function valida(input) {
+    let errors = {}
+    if (!input.name) {
+        errors.name = "Name required"
+    } else if (!input.duration) {
+        errors.duration = "Duration required"
+    } else if (!/\b([1-9]|1[0-9]|2[0-4])\b/.test(input.duration)) {
+        errors.duration = "Invalid number. Between 1-24"
+    }
+    return errors;
+}
+
 function AddActivity() {
 
     const dispatch = useDispatch()
     const history = useHistory()
     const countries = useSelector(state => state.countries)
+    const [errors, setErrors] = useState({})
+
+    const [activeButton, setActiveButton] = useState(false)
 
     const [input, setInput] = useState({
         name: '',
@@ -18,6 +33,8 @@ function AddActivity() {
         season: '',
         countries: []
     })
+
+    console.log(input)
 
     useEffect(() => {
         dispatch(getCountries())
@@ -32,14 +49,22 @@ function AddActivity() {
             ...input,
             [e.target.name]: e.target.value
         })
+        setErrors(valida({
+            ...input,
+            [e.target.name]: e.target.value
+        }))
     }
 
     function handleSelect(e) {
-        setInput({
-            ...input,
-            countries: [...input.countries, e.target.value]
-        })
+            setInput({
+                ...input,
+                countries: [...input.countries, e.target.value]
+            })
     }
+
+    const filterRepeatedCountries = [...new Set(input.countries.map(e => e))]
+
+    console.log(filterRepeatedCountries)
 
     function handleCheckSeason(e) {
         if (e.target.checked) {
@@ -78,6 +103,7 @@ function AddActivity() {
         history.push('/countries')
     }
 
+
     const season = ['Summer', 'Autumn', 'Winter', 'Spring'];
     const difficulty = [1, 2, 3, 4, 5];
 
@@ -89,53 +115,62 @@ function AddActivity() {
                     <h1>Add Activity</h1>
                     <div>
                         <form onSubmit={handleSubmit}>
-                            <div className={style.activity}>
+                            <div>
                                 <label>Activity: </label>
-                                <input type="text" value={input.name} name="name" onChange={handleChange} className={style.input} required />
+                                <input type="text" value={input.name} name="name" onChange={handleChange} placeholder="Activity name..." required />
+                                {errors.name && (
+                                    <p className="error">{errors.name}</p>
+                                )}
                             </div>
 
-                            <div className={style.activity}>
+                            <div>
                                 <label>Season: </label>
                                 {season.map(e => (
-                                    <label className={style.checkboxContainer}>
+                                    <label>
                                         <input type="radio" value={e} name="season" onChange={handleCheckSeason} required />
                                         {e}
                                     </label>
                                 ))}
                             </div>
 
-                            <div className={style.activity}>
+                            <div>
                                 <label>Difficulty: </label>
-                                <select onChange={handleSelctDifficulty} required>
+                                <select onChange={handleSelctDifficulty} required >
+                                    <option value="">Seleccione una opcion</option>
                                     {difficulty.map(e => (
                                         <option value={e} name="difficulty">{e}</option>
                                     ))}
                                 </select>
                             </div>
 
-                            <div className={style.activity}>
+                            <div>
                                 <label>Duration: </label>
-                                <input type="text" value={input.duration} name="duration" onChange={handleChange} className={style.input} required />
+                                <input type="text" value={input.duration} name="duration" onChange={handleChange} required placeholder="Hours...(only number)" />
+                                {errors.duration && (
+                                    <p>{errors.duration}</p>
+                                )}
                             </div>
 
-                            <div className={style.activity}>
+                            <div>
                                 <label>Country: </label>
-                                <select onChange={handleSelect} required>
+                                <select onChange={handleSelect} required >
+                                    <option value="">Seleccione una opcion</option>
                                     {countries.map(e => (
                                         <option value={e.id} name="countries" key={e.id} >{e.name}</option>
                                     ))}
                                 </select>
                             </div>
                             <div>
-                            <ul>
-                                <li className={style.countriesSelected}>{input.countries.map(i =>
-                                    <div>
-                                        {i}
-                                        <button onClick={() => handleDelete(i)} type="button">-</button>
-                                    </div>)}</li>
-                            </ul>
+                                <ul>
+                                    <li className={style.countriesSelected}>{filterRepeatedCountries.map(i =>
+                                        <div>
+                                            {i}
+                                            <button onClick={() => handleDelete(i)} type="button">X</button>
+                                        </div>)}</li>
+                                </ul>
                             </div>
-                            <button type="submit">submit</button>
+                            <button type="reset">Reset</button>
+                            <button type="submit">Add</button>
                         </form>
                     </div>
                 </div>
