5d15f901d52e07f62fbdc1257e7c6e2f874d59d2
Renata Loustalet <renataloustalet@hotmail.com>
2022-03-08 01:30:26 -0300

    modifique el estilo del componente AddActivity

--- client/src/components/AddActivity.jsx
+++ client/src/components/AddActivity.jsx
@@ -107,7 +107,7 @@ function AddActivity() {
         history.push('/')
     }
 
-    const season = ['Summer', 'Autumn', 'Winter', 'Spring'];
+    const season = ['Winter', 'Spring', 'Autumn', 'Summer'];
     const difficulty = [1, 2, 3, 4, 5];
     const duration = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
 
